93ad3d7c28228943aba2782bc964240e0caf0c4f
bragelbytes <joebragel@gmail.com>
2024-02-16 14:02:15 -0800

    Update ExampleInstrumentedTest.kt

--- CanaryLibrary/src/androidTest/java/org/OperatorFoundation/CanaryLibrary/ExampleInstrumentedTest.kt
+++ CanaryLibrary/src/androidTest/java/org/OperatorFoundation/CanaryLibrary/ExampleInstrumentedTest.kt
@@ -11,14 +11,4 @@ class ExampleInstrumentedTest {
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
         assertEquals("org.OperatorFoundation.CanaryLibrary.test", appContext.packageName)
     }
-
-//    @Test
-//    fun createCanaryInstance() = runTest {
-//        // ***** This is the actual API for the Canary Library ******
-//        val storageDirectory = Environment.getStorageDirectory()
-//        val canary = Canary(configDirectory = storageDirectory, saveDirectory = storageDirectory)
-//        assertNotNull(canary)
-//        canary.runTest()
-//    }
-
 }
